20329f8e2386575aad5c88afc29213423c581451
Søren Enggaard Hansen <monobryn64@gmail.com>
2013-12-30 18:46:06 +0100

    Added downsampling as a signal processing example

--- ear.hs
+++ ear.hs
@@ -1,5 +1,15 @@
-import Data.WAVE (hGetWAVE, waveSamples)
+import Data.WAVE
+-- (hGetWAVE, waveSamples, hPutWAVE, WAVESample)
 import System.IO
 import DSP.Basic
+import Numeric.Transform.Fourier.FFT
+import Data.Array
+import Data.Complex
 
-main = (fmap (concat . waveSamples) $ hGetWAVE stdin) >>= print
+main = do
+    wave <- hGetWAVE stdin
+    let
+        signal = map head $ waveSamples wave :: [WAVESample]
+        header = waveHeader wave
+        frameRate = waveFrameRate header in
+        hPutWAVE stdout $ wave {waveSamples = map (:[]) $ downsample 3 signal, waveHeader = header {waveFrameRate = div frameRate 3}}
